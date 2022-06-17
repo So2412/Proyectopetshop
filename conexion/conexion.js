f96816990fa2a6c412e105eb8dda8e7f4939d9bb
HP <HP@DESKTOP-VE0PJSL.com>
2022-06-17 00:51:07 -0500

    despliegue a heroku

--- conexion/conexion.js
+++ conexion/conexion.js
@@ -1,8 +1,17 @@
-const mysql =require('mysql')
+/*const mysql =require('mysql')
 module.exports=()=>
 mysql.createConnection({
     host:'localhost',
     user:'root',
     password: '',
     database:'dbpetshop'
+})*/
+
+const mysql =require('mysql')
+module.exports=()=>
+mysql.createConnection({
+    host:'bcvunhiwtyplpr3ieoib-mysql.services.clever-cloud.com',
+    user:'uqtme5xrgmpsqtph',
+    password:'qQ0YqbP0Zxtphk55QecE',
+    database:'bcvunhiwtyplpr3ieoib'
 })
